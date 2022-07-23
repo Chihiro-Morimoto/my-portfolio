c7ac4b555dbd682d5e1ba4e76b9c0562792c105b
ChihiroMorimoto <k10fwnjsk7867.wfty@gmail.com>
2022-07-23 15:30:28 +0900

    ポートフォリオサイトの開発を進めました

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
+const sameBirthday = document.getElementById("same-birthday");
+
 window.onload = function () {
-  alert("Welcome to My Portfolio Site!!");
+  alert("見に来てくれてありがとう！");
+};
+
+sameBirthday.onclick = function () {
+  sameBirthday.textContent =
+    "A.あ～ちゃん(Perfume)、丹生明里(日向坂46)、山崎方正etc...";
 };
